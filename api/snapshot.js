79cef7d8b2855fafac663fc861a687bf4f95f126
spharb <spencerharbold@gmail.com>
2025-04-24 11:23:57 -0600

    Update snapshot.js

--- api/snapshot.js
+++ api/snapshot.js
@@ -1,16 +1,18 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
-    const { DD_API_KEY, DD_APP_KEY } = process.env;
-  
-    const response = await fetch("https://api.datadoghq.com/api/v1/graph/snapshot", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "DD-API-KEY": DD_API_KEY,
-        "DD-APPLICATION-KEY": DD_APP_KEY
-      },
-      body: JSON.stringify(req.body)
-    });
-  
-    const data = await response.json();
-    res.status(response.status).json(data);
-  }
+  const { DD_API_KEY, DD_APP_KEY } = process.env;
+
+  const response = await fetch("https://api.datadoghq.com/api/v1/graph/snapshot", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "DD-API-KEY": DD_API_KEY,
+      "DD-APPLICATION-KEY": DD_APP_KEY
+    },
+    body: JSON.stringify(req.body)
+  });
+
+  const data = await response.json();
+  res.status(response.status).json(data);
+}
